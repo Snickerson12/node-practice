848624e3af784b94e8625a963ece2f4b216af17f
Snickerson12 <laura.nickerson@gmail.com>
2020-02-12 11:56:33 -0600

    examples of node global object native methods

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+// in node the global object is not just the window, it includes native methods 
+// some of these methods include console, setTimeout, setInterval, require, exports, etc
+
 console.log('hello via node')
 
 setTimeout(function() {
     console.log('2 seconds have passed');
 }, 2000);
+
+var time = 0
+
+setInterval(function(){
+    time += 2;
+    console.log(time + ' seconds have passed')
+}, 2000)
+
+var timer = setInterval(function() {
+    time += 2;
+    console.log(time + ' seconds have passed');
+    if (time > 5) {
+        clearInterval(timer);
+    }
+}, 2000);
+
+// states current directory
+console.log(__dirname);
+
+// states current file path
+console.log(__filename);
+
+//require, exports
